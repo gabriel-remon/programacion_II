d9bdc6f6d3d34cca94e6204759f95e8327649106
gabriel-remon <remongabi16@gmail.com>
2022-05-31 21:27:01 -0300

    º

---
+++ ejerciciosDeClases/clase12- tipos genericos/EjercicioI01 (torneo)/Biblioteca/Equipo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public abstract class Equipo
+    {
+        protected string nombre;
+        protected DateTime fechaCreacion;
+
+        public Equipo(string nombre, DateTime fechaCreacion)
+        {
+            this.nombre = nombre;
+            this.fechaCreacion = fechaCreacion;
+        }
+
+        public override string ToString()
+        {
+            return nombre;
+        }
+
+        public string Firma()
+        {
+            return $"{nombre} fundado el {fechaCreacion}";
+        }
+
+        public static bool operator == (Equipo equipo1, Equipo equipo2) 
+        {
+            return equipo1.nombre == equipo2.nombre && equipo1.fechaCreacion == equipo2.fechaCreacion;
+        }
+        public static bool operator != (Equipo equipo1, Equipo equipo2) 
+        {
+            return !(equipo1 == equipo2);
+        }
+
+    }
+}
---
+++ ejerciciosDeClases/clase12- tipos genericos/EjercicioI01 (torneo)/Biblioteca/Torneo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Torneo<T> where T : Equipo
+
+    {
+        private string nombre;
+        private List<T> equipos;
+
+        public Torneo()
+        {
+            equipos = new List<T>();
+        }
+
+        public Torneo(string nombre)
+            :this()
+        {
+            this.nombre = nombre;
+        }
+
+
+        public string JugarPartido
+        {
+           get
+           {
+                Random random = new Random();
+                int equipo1 = random.Next(0,equipos.Count);
+                int equipo2 = random.Next(0,equipos.Count);
+
+                return CalcularPartido(equipos[equipo1], equipos[equipo2]);
+           }
+        }
+
+        public string mostrar()
+        {
+            StringBuilder retorno = new StringBuilder();
+
+            retorno.AppendLine(nombre);
+            foreach(Equipo unEquipo in equipos)
+            {
+                retorno.AppendLine($"1 - {unEquipo.Firma()}");
+            }
+            return retorno.ToString();
+        }
+
+        public string CalcularPartido(T equipo1, T equipo2)
+        {
+            Random random = new Random();
+            
+            if(equipo1 is Equipo && equipo2 is Equipo)
+            {
+                return $"{equipo1} {random.Next(0, 10)} -" +
+                         $" {random.Next(0, 10)} {equipo2}";
+            }
+
+            return "";
+        }
+
+        public static bool operator ==(Torneo<T> torneo , Equipo equipo)
+        {
+                foreach (T unEquipo in torneo.equipos)
+                {
+                    if (unEquipo == equipo)
+                        return false;
+                }
+            return true;
+          
+        }
+
+        public static bool operator !=(Torneo<T> torneo, Equipo equipo)
+        {
+            return !(torneo == equipo);
+        }
+
+        public static bool operator + (Torneo<T> torneo, Equipo equipo)
+        {
+            if (torneo != equipo)
+            {
+                torneo.equipos.Add((T)equipo);
+                return true;
+            }
+
+            return false;
+            
+        }
+
+    }
+}
